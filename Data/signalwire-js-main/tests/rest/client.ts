d05edfdad489a20bd6c6b8478d0c48ef695c0675
evan-hopson <evan.hopson@gmail.com>
2022-10-17 10:42:29 -0400

    delete

--- Data/signalwire-js-main/tests/rest/client.ts
+++
@@ -1,8 +0,0 @@
-import RestClient from '../../src/rest'
-
-const host = 'example.signalwire.com'
-const project = 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX'
-const token = 'PTXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
-
-const client = RestClient(project, token, { signalwireSpaceUrl: host })
-export default client
